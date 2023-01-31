da4f6d6fcce69031f7589256f492c31c248f5402
Omar EL-Sheikh <omarsheikh6666@gmail.com>
2023-01-31 23:09:26 +0200

    Sample Commit

---
+++ Mini_Project_4_Eclipse_WS/Mini_Project_4/Debug/Application.d
@@ -0,0 +1,10 @@
+Application.o Application.o: ../Application.c ../lcd.h ../std_types.h \
+  ../ultrasonic.h ../gpio.h
+
+../lcd.h:
+
+../std_types.h:
+
+../ultrasonic.h:
+
+../gpio.h:
---
+++ Mini_Project_4_Eclipse_WS/Mini_Project_4/Debug/ultrasonic.d
@@ -0,0 +1,14 @@
+ultrasonic.o ultrasonic.o: ../ultrasonic.c ../ultrasonic.h ../std_types.h \
+  ../gpio.h ../icu.h ../lcd.h ../common_macros.h
+
+../ultrasonic.h:
+
+../std_types.h:
+
+../gpio.h:
+
+../icu.h:
+
+../lcd.h:
+
+../common_macros.h:
